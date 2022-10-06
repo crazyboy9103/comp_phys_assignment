62cdd3d3a249b9c79bf9e0d4a6b22c3a74bd8ab8
crazyboy9103 <59700837+crazyboy9103@users.noreply.github.com>
2022-10-06 11:44:26 +0900

    Update and rename 1. machine accuracy.py to machine_acc.py

--- machine_acc.py
+++ machine_acc.py
@@ -1,18 +1,16 @@
 ##########################1#########################
-#a#
+# a) 
 def floating(number):
     n =number
 
     while 1.0+n != 1.0:
         print(n)
-        n = n/2
-#divide by 2 it's binary system
+        n = n/2 #divide by 2 it's binary system
         continue
     print(n)
         
 floating(1000)
 
-#the input can be changed arbitrarily
-
-#b#
+#the input can be set arbitrarily
+# b)
 '''Machine accuracy was 1E-16'''
